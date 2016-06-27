f7ce7f5f9dc0414e95c69d49a2a82cd94ab7b343
zhale001 <zachary.a.hale@gmail.com>
2016-06-26 19:34:36 -0500

    app created, form-based workflow added

---
+++ blogger/app/controllers/articles_controller.rb
@@ -0,0 +1,46 @@
+class ArticlesController < ApplicationController
+	def index
+		@articles = Article.all
+		
+	end
+	def show
+		@article = Article.find(params[:id])
+	end
+	def new
+		@article = Article.new
+	end
+	def create
+		@article = Article.new(article_params)
+
+		@article.save
+		flash.notice = "blogging '#{@article.title}' created"
+		redirect_to article_path(@article)
+	end
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		flash.notice = "blogging '#{@article.title}' deleted"
+		redirect_to articles_path
+		
+	end
+	def edit
+		@article = Article.find(params[:id])
+		
+	end
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+
+		flash.notice = "blogging '#{@article.title}' updated"
+		redirect_to article_path(@article)
+		
+	end
+
+	private
+
+	def article_params
+		params.require(:article).permit(:title, :body)
+		
+	end
+end
+ 
