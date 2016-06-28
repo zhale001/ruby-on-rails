61320b9c84cfc638136d0dea40a956f6207601bb
zhale001 <zachary.a.hale@gmail.com>
2016-06-27 19:10:06 -0500

    added paperclip gem

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -43,7 +43,7 @@ class ArticlesController < ApplicationController
 	private
 
 	def article_params
-		params.require(:article).permit(:title, :body, :tag_list)
+		params.require(:article).permit(:title, :body, :tag_list, :image)
 		
 	end
 end
